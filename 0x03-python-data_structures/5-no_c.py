29e305a154b9a48c8460e99add958d51d14a7882
Rigiih7 <mwirigihbrian@gmail.com>
2023-08-14 11:58:57 -0700

    remove c correction1

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 # 5-no_c.py
 
 def no_c(my_string):
-    newstring = [x for x in my_string if x != 'c' and x != 'C']
-    return("".join(newstring))
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return("".join(copy))
