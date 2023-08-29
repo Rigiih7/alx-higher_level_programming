88e53c2c11171e81b638e1503f08af1b074b725c
Rigiih7 <mwirigihbrian@gmail.com>
2023-08-29 11:15:03 -0700

    square with sizer

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class Square."""
+
+
+class Square:
+    """class square."""
+
+    def __init__(self, size):
+        """initialize a new object Square.
+
+        Args:
+            size (int): Size of the new square.
+        """
+        self.__size = size
