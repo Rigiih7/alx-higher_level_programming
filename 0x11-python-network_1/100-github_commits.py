f32cea5d190ac330afdc6a9fa600f50ee081875e
Rigiih7 <mwirigihbrian@gmail.com>
2023-12-05 14:16:54 -0800

    List commit using github API

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes 2 arguments to 
+list 10 commits (from the most recent to oldest) 
+of the repository “rails” by the user “rails”
+
+The first argument will be the repository name
+The second argument will be the owner name
+You must use the packages requests and sys
+You are not allowed to import packages other than requests and sys
+You don’t need to check arguments passed to the script (number or type)
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    try:
+        repo_name = sys.argv[1]
+        username = sys.argv[2]
+        commmits_url = "https://api.github.com/repos/{}/{}/commits" \
+            .format(username, repo_name)
+        response = requests.get(commmits_url)
+        json_obj = response.json()
+        for i, obj in enumerate(json_obj):
+            if i == 10:
+                break
+            if type(obj) is dict:
+                name = obj.get('commit').get('author').get('name')
+                print("{}: {}".format(obj.get('sha'), name))
+    except ValueError as invalid_json:
+        pass
