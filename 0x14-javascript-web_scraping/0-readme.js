cbef0632160100359f68fd1a12eb2632b08e5ea9
Rigiih7 <mwirigihbrian@gmail.com>
2024-01-04 08:00:39 -0800

    Readme, Writeme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
-const process = require('process');
+
 const fs = require('fs');
 
-
-const file = process.argv[2];
-fs.readFile(file, 'utf8', function (err, data) {
-  // If an error occurred during the reading, print the error object
-  if (err) {
-    console.log(err);
-  } else {
-    process.stdout.write(data);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
